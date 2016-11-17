fdf3e0d85fb29ec976afc7146526a9a8b3cad333
Ali Issa <alisissa@gmail.com>
2016-11-17 13:45:17 +0200

    making queueable jobs work with 5.3

--- src/QueueableJob.php
+++ src/QueueableJob.php
@@ -5,6 +5,7 @@ namespace Lucid\Foundation;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Bus\Queueable;
 
 /**
  * An abstract Job that can be managed with a queue
@@ -14,4 +15,5 @@ class QueueableJob extends Job implements ShouldQueue
 {
     use SerializesModels;
     use InteractsWithQueue;
+    use Queueable;
 }
