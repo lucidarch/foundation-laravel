def74b980bb0fb6fdab3ef87ffa265a3e1355d6b
Alex Giuvara <alex@redloyalty.nl>
2017-04-01 17:49:04 +0200

    Add Laravel 5.4 api and web routes

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -2,24 +2,74 @@
 
 namespace Lucid\Foundation\Providers;
 
-use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as BaseServiceProvider;
+use Illuminate\Routing\Router;
 
 abstract class RouteServiceProvider extends BaseServiceProvider
 {
     /**
-     * Read the routes from the "routes.php" file of this Service
+     * Read the routes from the "api.php" and "web.php" files of this Service
      *
      * @param \Illuminate\Routing\Router $router
      */
     abstract public function map(Router $router);
 
-    public function loadRoutesFile($router, $namespace, $path)
+    /**
+     * @param $router
+     * @param $namespace
+     * @param $pathApi
+     * @param $pathWeb
+     */
+    public function loadRoutesFiles($router, $namespace, $pathApi = null, $pathWeb = null)
     {
-        $router->group(['namespace' => $namespace], function ($router) use ($path) {
-            if (file_exists($path)) {
-                require $path;
-            }
+        if (is_file($pathApi)) {
+            $this->mapApiRoutes($router, $namespace, $pathApi);
+        }
+        if (is_file($pathWeb)) {
+            $this->mapWebRoutes($router, $namespace, $pathWeb);
+        }
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @param $router
+     * @param $namespace
+     * @param $path
+     *
+     * @return void
+     */
+    protected function mapApiRoutes($router, $namespace, $path)
+    {
+        $router->group([
+            'middleware' => 'api',
+            'namespace'  => $namespace,
+            'prefix'     => 'api'
+        ], function ($router) use ($path) {
+            require $path;
+        });
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @param $router
+     * @param $namespace
+     * @param $path
+     *
+     * @return void
+     */
+    protected function mapWebRoutes($router, $namespace, $path)
+    {
+        $router->group([
+            'middleware' => 'web',
+            'namespace'  => $namespace
+        ], function ($router) use ($path) {
+            require $path;
         });
     }
 }
