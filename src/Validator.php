1501f55fd9b553c27e1bacdf42c8f8d8cefff797
Charalampos Raftopoulos <harrisrafto@gmail.com>
2018-07-17 16:37:43 +0300

    accept custom validation messages

--- src/Validator.php
+++ src/Validator.php
@@ -24,14 +24,15 @@ class Validator
      *
      * @param array $input The input to validate
      * @param array $rules Specify custom rules (will override class rules)
+     * @param array $messages Specify custom messages (will override class messages)
      *
      * @return bool
      *
      * @throws \Lucid\Foundation\InvalidInputException
      */
-    public function validate(array $input, array $rules = [])
+    public function validate($input, array $rules = [], array $messages = [])
     {
-        $validation = $this->validation($input, $rules);
+        $validation = $this->validation($input, $rules, $messages);
 
         if ($validation->fails()) {
             throw new InvalidInputException($validation);
@@ -46,15 +47,16 @@ class Validator
      *
      * @param array $input
      * @param array $rules
+     * @param array $messages
      *
      * @return \Illuminate\Validation\Validator
      */
-    public function validation(array $input, array $rules = [])
+    public function validation($input, array $rules = [], array $messages = [])
     {
         if (empty($rules)) {
             $rules = $this->rules;
         }
 
-        return $this->validation->make($input, $rules);
+        return $this->validation->make($input, $rules, $messages);
     }
 }
