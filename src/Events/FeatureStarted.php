e12186fb65f4580f40f750dfba080c32d05d22f8
Alexander Diachenko <adiach3nko@gmail.com>
2019-08-22 16:20:08 +0300

    Log op arguments along with class FQN

--- src/Events/FeatureStarted.php
+++ src/Events/FeatureStarted.php
@@ -10,11 +10,18 @@ class FeatureStarted
     public $name;
 
     /**
+     * @var array
+     */
+    public $arguments;
+
+    /**
      * FeatureStarted constructor.
      * @param  string  $name
+     * @param  array  $arguments
      */
-    public function __construct($name)
+    public function __construct($name, array $arguments = [])
     {
         $this->name = $name;
+        $this->arguments = $arguments;
     }
 }
