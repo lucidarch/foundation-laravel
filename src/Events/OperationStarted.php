29d12df3783b78f823713adaae26c760f2d945eb
Alexander Diachenko <adiach3nko@gmail.com>
2019-08-22 16:20:08 +0300

    Log op arguments along with class FQN

--- src/Events/OperationStarted.php
+++ src/Events/OperationStarted.php
@@ -10,11 +10,18 @@ class OperationStarted
     public $name;
 
     /**
+     * @var array
+     */
+    public $arguments;
+
+    /**
      * OperationStarted constructor.
      * @param  string  $name
+     * @param  array  $arguments
      */
-    public function __construct($name)
+    public function __construct($name, array $arguments = [])
     {
         $this->name = $name;
+        $this->arguments = $arguments;
     }
 }
